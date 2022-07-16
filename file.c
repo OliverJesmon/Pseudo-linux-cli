400650c0806badbd7d57610f061382f7e9d9b56e
OliverJesmon <102456180+OliverJesmon@users.noreply.github.com>
2022-07-16 12:58:27 +0530

    file.c

---
+++ file.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+
+int main() {
+    // Write C code here
+    char li[70];
+    int i;
+    int j;
+    for(i=0;i<j;j++)
+    {
+    printf("[root@program/]$");
+    scanf("%s",li);
+}
+    
+    
+    return 0;
+}
