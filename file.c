92c13af76e58afa0b2b57e3cc00883d5f0947927
OliverJesmon <102456180+OliverJesmon@users.noreply.github.com>
2022-07-31 16:23:45 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,16 +1,18 @@
 include <stdio.h>
 
-int main() {
+void main() {
     // Write C code here
     char li[70];
     int i;
     int j;
+    char nme[7],pwd[7];
+    printf("\nEnter name:\t");
+    scanf("%s",nme);
+    printf("\nPassword:");
+    scanf("%s",pwd);
     for(i=0;i<j;j++) //Loop to create infinite series of input output statement
     {
-    printf("[root@localhost/]$");
+    printf("[%s@localhost/]$",nme);
     scanf("%s",li);
 }
-    
-    
-    return 0;
 }
